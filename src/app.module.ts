8999e5f748fe93e6bdfd46db1223918b24379fb0
luizcod <dudu.nantes@gmail.com>
2026-06-05 20:21:01 -0300

    feat: crud de usuários com Prisma e NestJS

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { ProductsModule } from './products/products.module';
-import { AvaliacoesProdutoModule } from './avaliacoes-produto/avaliacoes-produto.module';
-import { AvaliacoesLojaModule } from './avaliacoes-loja/avaliacoes-loja.module';
-import { CategoriasModule } from './categorias/categorias.module';
-import { LojasModule } from './lojas/lojas.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    PrismaModule,
-    ProductsModule,
-    AvaliacoesProdutoModule,
-    AvaliacoesLojaModule,
-    CategoriasModule,
-    LojasModule,
-  ],
+  imports: [UsersModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
