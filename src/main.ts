bbe91fbf264c43c097c080f06c379351112987e8
luizcod <dudu.nantes@gmail.com>
2026-06-20 11:24:45 -0300

    atualizando integração cadastro e perfil

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const uploadsPath = join(process.cwd(), 'uploads');
+  console.log('Servindo arquivos estáticos de:', uploadsPath);
+
+  app.useStaticAssets(uploadsPath, {
+    prefix: '/uploads/',
+  });
+
   app.enableCors({
-    origin: 'http://localhost:3000', 
+    origin: 'http://localhost:3000',
     credentials: true,
   });
-  await app.listen(process.env.PORT ?? 3001);
-
+  await app.listen(3333);
 }
 bootstrap();
-
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,7 @@
-import { Controller, Get, Post, Body, Patch, Param, UnauthorizedException, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, UnauthorizedException, Delete, UseInterceptors, UploadedFile } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -23,6 +26,28 @@ export class UsersController {
     if (!senhaCorreta) throw new UnauthorizedException('Credenciais inválidas');
     const { senha_hash, ...dados } = usuario;
     return dados;
+  }
+
+  //rota de upload de avatar
+  @Post(':id/avatar')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads/avatars',
+        filename: (req, file, callback) => {
+          const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const ext = extname(file.originalname);
+          callback(null, `avatar-${uniqueSuffix}${ext}`);
+        },
+      }),
+    }),
+  )
+  async uploadAvatar(
+    @Param('id') id: string,
+    @UploadedFile() file: Express.Multer.File,
+  ) {
+    const url = `http://localhost:3333/uploads/avatars/${file.filename}`;
+    return this.usersService.update(+id, { foto_perfil_url: url });
   }
 
   @Get()
